2236b86cc8ad95302b4f4e14694de04bd8a7ffa5
roshan nawkar <roshannawkar2807@gmail.com>
2026-02-08 17:24:32 +0530

    responsive-add

--- src/components/windows/Resume.jsx
+++ src/components/windows/Resume.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import MacWindow from './MacWindow'
-import "./resume.scss"
+import React from "react";
+import MacWindow from "./MacWindow";
+import "./resume.scss";
 
 const Resume = ({ windowName, setWindowsState }) => {
-    return (
-        <MacWindow windowName={windowName} setWindowsState={setWindowsState} >
-            <div className="resume-window">
-                <embed src="/resume.pdf" frameborder="0"></embed>
-            </div>
-        </MacWindow>
-    )
-}
+  return (
+    <MacWindow windowName={windowName} setWindowsState={setWindowsState}>
+      <div className="resume-window">
+        <embed src="/resume (2).pdf" frameborder="0"></embed>
+      </div>
+    </MacWindow>
+  );
+};
 
-export default Resume
+export default Resume;
