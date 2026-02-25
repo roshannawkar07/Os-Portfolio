c1c5a0a3c1c4e9cfce524219b5ab8a0a02f52a2b
roshan nawkar <roshannawkar2807@gmail.com>
2026-02-25 15:47:28 +0530

    pending

--- src/components/windows/MacWindow.jsx
+++ src/components/windows/MacWindow.jsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Rnd } from "react-rnd";
 import "./window.scss";
 
 const MacWindow = ({
   children,
-  width = "40vw",
-  height = "40vh",
+  width = "45vw",
+  height = "55vh",
   windowName,
   setWindowsState,
 }) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 480);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 480);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <Rnd
       default={{
-        width: width,
-        height: height,
-        x: 300,
-        y: 200,
+        width: isMobile ? "95vw" : width,
+        height: isMobile ? "80vh" : height,
+
+        // 👉 Position change here
+        x: isMobile ? 20 : 300,
+        y: isMobile ? 200 : 200,
       }}
     >
       <div className="window">
@@ -23,7 +36,10 @@ const MacWindow = ({
           <div className="dots">
             <div
               onClick={() =>
-                setWindowsState((state) => ({ ...state, [windowName]: false }))
+                setWindowsState((state) => ({
+                  ...state,
+                  [windowName]: false,
+                }))
               }
               className="dot red"
             ></div>
@@ -35,6 +51,7 @@ const MacWindow = ({
             <p>roshan_nawkar</p>
           </div>
         </div>
+
         <div className="main-content">{children}</div>
       </div>
     </Rnd>
